be60da3ab03b43d998ac05a6ba10a62b8ca26319
MiauMaton <maxuribefalcon@hotmail.com>
2021-10-19 13:27:44 -0600

    Update Problem58.java

--- Problem58.java
+++ Problem58.java
@@ -1,9 +1,16 @@
+/*
+Given a string s consisting of some words 
+separated by some number of spaces, 
+return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space 
+characters only.
+*/
 class Problem58 {
 	public int lengthOfLastWord(String s) {
+		//saving the word separated by a space in a array
         String[] words = s.split(" ");
-    
-        String wordToCount = words[words.length-1];
-        
-        return wordToCount.length();
+    	//returning the length of the last word
+        return words[words.length-1].length();
     }
 }
