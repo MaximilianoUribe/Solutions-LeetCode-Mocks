c21f118cc1377a0fa4a18b671bf1e7262de839df
MiauMaton <maxuribefalcon@hotmail.com>
2022-01-01 12:07:04 -0700

    Create Problem226.java

---
+++ Problem226.java
@@ -0,0 +1,34 @@
+/*
+invert the subtrees of the binary tree
+*/
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+*/
+
+class Problem226 {
+	public TreeNode invertTree(TreeNode root) {
+		if(root == null) return null;
+
+		TreeNode temp = root.right;
+		root.right = root.left;
+		root.left = temp;
+
+		invertTree(root.right);
+		invertTree(root.left);
+
+		return root;
+	}
+}
